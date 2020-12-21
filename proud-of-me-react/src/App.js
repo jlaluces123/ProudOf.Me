0811591b93ad98a3c38158526d3fe79a3851429b
Jonathan Laluces <jlalucescareer123@gmail.com>
2020-12-21 10:55:56 -0500

    Accessing Profile: react-app now gets profile information from backend

--- proud-of-me-react/src/App.js
+++ proud-of-me-react/src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import Home from './components/home';
+import Profile from './components/profile';
+import Login from './components/login';
 
 function App() {
     return (
         <div className='App'>
-            <header className='App-header'>
-                <img src={logo} className='App-logo' alt='logo' />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className='App-link'
-                    href='https://reactjs.org'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                >
-                    Learn React
-                </a>
-            </header>
+            <Switch>
+                <Route exact path='/' component={Home} />
+                <Route path='/user/:userId' component={Profile} />
+                <Route path='/login' component={Login} />
+            </Switch>
         </div>
     );
 }
---
+++ proud-of-me-react/src/components/profile.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Profile = () => {
+    const [id, setId] = useState();
+    const [user, setUser] = useState();
+
+    useEffect(() => {
+        if (!id || !user) {
+            let url = window.location.pathname;
+            url.split('/');
+            console.log(url.split('/')[2]);
+            setId(url.split('/')[2]);
+
+            async function getProfile() {
+                let userId = id;
+                await axios
+                    .get(
+                        `https://proud-of-me-backend.herokuapp.com/api/user/${userId}`
+                    )
+                    .then((response) => {
+                        console.log(Object.values(response.data.user)[0]);
+                        setUser(Object.values(response.data.user)[0]);
+                    })
+                    .catch((err) => console.log(err));
+            }
+
+            getProfile();
+        }
+    }, []);
+
+    return (
+        <div>
+            {user ? (
+                <h1>Welcome: {user.username}</h1>
+            ) : (
+                <h1>No User Found Yet</h1>
+            )}
+        </div>
+    );
+};
+
+export default Profile;
