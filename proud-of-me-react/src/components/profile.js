f5cfaa11b494d592459ef74660e3f56788b62880
Jonathan Laluces <jlalucescareer123@gmail.com>
2020-12-21 11:04:06 -0500

    Logged In Bug: fixed issue where refreshing the profile page wouldn't keep track of the id used to get our profile information

--- proud-of-me-react/src/components/profile.js
+++ proud-of-me-react/src/components/profile.js
@@ -11,27 +11,31 @@ const Profile = () => {
             url.split('/');
             console.log(url.split('/')[2]);
             setId(url.split('/')[2]);
-
-            async function getProfile() {
-                let userId = id;
-                await axios
-                    .get(
-                        `https://proud-of-me-backend.herokuapp.com/api/user/${userId}`
-                    )
-                    .then((response) => {
-                        console.log(Object.values(response.data.user)[0]);
-                        setUser(Object.values(response.data.user)[0]);
-                    })
-                    .catch((err) => console.log(err));
-            }
-
-            getProfile();
         }
     }, []);
 
+    useEffect(() => {
+        console.log('ID has changed: ', id);
+        async function getProfile() {
+            let userId = id;
+            console.log('User ID: ', userId);
+            await axios
+                .get(
+                    `https://proud-of-me-backend.herokuapp.com/api/user/${userId}`
+                )
+                .then((response) => {
+                    console.log(Object.values(response.data.user)[0]);
+                    setUser(Object.values(response.data.user)[0]);
+                })
+                .catch((err) => console.log(err));
+        }
+
+        getProfile();
+    }, [id]);
+
     return (
         <div>
-            {user ? (
+            {id && user ? (
                 <h1>Welcome: {user.username}</h1>
             ) : (
                 <h1>No User Found Yet</h1>
