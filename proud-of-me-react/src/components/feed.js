1ee8de9fb279348262fb1ea5c1b9d473780a17df
Jonathan Laluces <jlalucescareer123@gmail.com>
2021-02-08 11:18:31 -0500

    Feed Component: data pulled down from backend successful and stored in state

--- proud-of-me-react/src/components/feed.js
+++ proud-of-me-react/src/components/feed.js
@@ -1,6 +1,28 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
+/* 
+    This component will:
+        - Pull /feed data from backend
+        - Give title / story props to FeedCard component
+            + Map through API data and return FeedCard components
+*/
 const Feed = () => {
+    const [stories, setStories] = useState([]);
+
+    const getFeedData = () => {
+        axios
+            .get('http://localhost:3388/api/users/feed')
+            .then((data) => setStories(data.data.moments))
+            .catch((err) => console.error(err));
+    };
+
+    useEffect(() => {
+        getFeedData();
+    }, []);
+
+    useEffect(() => console.log('Stories Changed: ', stories), [stories]);
+
     return (
         <div>
             <section className='bg-blue-200'>
